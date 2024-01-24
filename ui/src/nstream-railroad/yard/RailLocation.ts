81595523d0fc7f339ef3e5c9d7d100e29907d824
ajay-gov <ajay.gov@protonmail.com>
2024-01-23 20:08:50 -0800

    Reduce stroke width and lighten the color of the rails

--- ui/src/nstream-railroad/yard/RailLocation.ts
+++ ui/src/nstream-railroad/yard/RailLocation.ts
@@ -1,7 +1,7 @@
 import { GeoPoint, GeoPath,Uri, Value, ValueDownlinkFastener } from "@swim/runtime";
 import { GeographicLine, LocationTrait} from "@swim/platform";
 
-const RAIL_LINE_WIDTH = 5;
+const RAIL_LINE_WIDTH = 1;
 
 export class RailLocation extends LocationTrait {
 
@@ -10,11 +10,11 @@ export class RailLocation extends LocationTrait {
     this.initLine(geoPoints);
   }
   
-  initLine(geoPoints: GeoPoint[]) {    0
+  initLine(geoPoints: GeoPoint[]) {    
     //const newLocal = latLngs as AnyGeoPath;
     const geographic = GeographicLine.fromInit({
       geometry: GeoPath.fromPoints(geoPoints),
-      stroke: "rgb(200, 200, 200)",
+      stroke: "rgb(100, 100, 100)",
       strokeWidth: RAIL_LINE_WIDTH
     });
     this.setGeographic(geographic);
